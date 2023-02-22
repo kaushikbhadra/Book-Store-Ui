f47839b4c60a402652cc3a7a0de6cb76ac28213b
Kaushik Bhadra <kbhadra100@gmail.com>
2023-02-22 21:32:25 +0530

    initial setup with httpclient

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  name!: string;
+  author!: string;
+  description!: string;
+  unitPrice!: number;
+  imageUrl!: string;
+  active!: boolean;
+  unitInStock!: number;
+  dateCreated!: Date;
+  lastUpdated!: Date;
+}
